8b9b42d2c45d9ff4d5489fed195e075884b9871f
adarapata <adarapata@e-f-b.jp>
2018-07-31 12:12:23 +0900

    first commit

---
+++ Assets/DebugPointerHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DebugPointerHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public Text _debugLabel;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _debugLabel.text = "Down!";
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _debugLabel.text = "Enter!";
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _debugLabel.text = "Exit!";
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _debugLabel.text = "Up!";
+    }
+}
